8bcec9228a1bd93453de2ab2d7ee66e2f4393210
Fernando Guirao <fernando@bueninvento.es>
2013-10-05 13:57:46 +0200

    Jerarquía de rutas terminada

--- app.js
+++ app.js
@@ -29,13 +29,48 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+/* ROUTES */
+
+/* Home */
 app.get('/', routes.index);
-app.get('/login', routes.login);
+app.get('/loged', routes.homeLoged);
+app.get('/unloged', routes.homeUnloged);
+/* Brands */
+app.get('/brands/brandName', routes.brands);
+app.get('/brands/brandName/edit', routes.brandsEdit);
+/* Events */
+app.get('/events/eventName', routes.events);
+app.get('/events/eventName/edit', routes.eventsEdit);
+/* Joinitys */
+app.get('/joinitys', routes.joinitys);
+app.get('/joinitys/create', routes.joinitysCreate);
+app.get('/joinitys/hobbies/hobbyName', routes.joinitysHobbiesHobby);
+app.get('/joinitys/hobbies/joinityName', routes.joinitysHobbiesJoinity);
+app.get('/joinitys/familyandfriends/joinityName', routes.joinitysFamilyandfriendsJoinity);
+app.get('/joinitys/shoppings/joinityName', routes.joinitysShoppingsJoinity);
+app.get('/joinitys/hobbies/joinityName/edit', routes.joinitysHobbiesJoinityEdit);
+app.get('/joinitys/familyandfriends/joinityName/edit', routes.joinitysFamilyandfriendsJoinityEdit);
+app.get('/joinitys/shoppings/joinityName/edit', routes.joinitysShoppingsJoinityEdit);
+/* Messages */
 app.get('/messages', routes.messages);
-app.get('/user/me', routes.user);
-app.get('/user/me/contacts', routes.usercontacts);
-app.get('/user/me/edit', routes.useredit);
-app.get('/users', user.list);
+/* Reservations */
+app.get('/reservations', routes.reservations);
+app.get('/reservations/manage', routes.reservationsManage);
+/* Shoppings */
+app.get('/shoppings', routes.shoppings);
+app.get('/shoppings/payment', routes.shoppingsPayment);
+app.get('/shoppings/purchases', routes.shoppingsPurchases);
+/* Tasks */
+app.get('/tasks', routes.tasks);
+/* Users */
+app.get('/user', routes.user);
+app.get('/user/userName', routes.userUser);
+app.get('/user/login', routes.userLogin);
+app.get('/user/contacts', routes.userContacts);
+app.get('/user/edit', routes.userEdit);
+app.get('/user/userName', user.list);
+
+/* END ROUTES */
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -3,26 +3,141 @@
  * GET home page.
  */
 
+/* HOME */
+
 exports.index = function(req, res){
-  res.render('pages/home/index', { title: 'Joinity' });
+  res.render('pages/home/loged/index', { title: 'Joinity' });
 };
 
-exports.login = function(req, res){
-  res.render('pages/login/index', { title: 'Login' });
+exports.homeLoged = function(req, res){
+  res.render('pages/home/loged/index', { title: 'Joinity' });
 };
+
+exports.homeUnloged = function(req, res){
+  res.render('pages/home/unloged/index', { title: 'Joinity' });
+};
+
+/* BRANDS */
+
+exports.brands = function(req, res){
+  res.render('pages/brands/index', { title: 'BrandName' });
+};
+
+exports.brandsEdit = function(req, res){
+  res.render('pages/brands/edit/index', { title: 'BrandName - Edit Brand' });
+};
+
+/* EVENTS */
+
+exports.events = function(req, res){
+  res.render('pages/events/index', { title: 'Events' });
+};
+
+exports.eventsEdit = function(req, res){
+  res.render('pages/events/edit/index', { title: 'Edit Event' });
+};
+
+/* JOINITYS */
+
+exports.joinitys = function(req, res){
+  res.render('pages/joinitys/index', { title: 'Joinitys' });
+};
+
+exports.joinitysCreate = function(req, res){
+  res.render('pages/joinitys/create/index', { title: 'Create joinity' });
+};
+
+exports.joinitysHobbiesHobby = function(req, res){
+  res.render('pages/joinitys/hobbies/categories/index', { title: 'Hobby Page' });
+};
+
+exports.joinitysHobbiesJoinity = function(req, res){
+  res.render('pages/joinitys/hobbies/index', { title: 'Hobby Page' });
+};
+
+exports.joinitysHobbiesJoinityEdit = function(req, res){
+  res.render('pages/joinitys/hobbies/edit/index', { title: 'Hobby Page Edit' });
+};
+
+exports.joinitysFamilyandfriendsJoinity = function(req, res){
+  res.render('pages/joinitys/familyandfriends/index', { title: 'Hobby Page' });
+};
+
+exports.joinitysFamilyandfriendsJoinityEdit = function(req, res){
+  res.render('pages/joinitys/familyandfriends/edit/index', { title: 'Hobby Page Edit' });
+};
+
+exports.joinitysShoppingsJoinity = function(req, res){
+  res.render('pages/joinitys/shoppings/index', { title: 'Shopping Page' });
+};
+
+exports.joinitysShoppingsJoinityEdit = function(req, res){
+  res.render('pages/joinitys/shoppings/edit/index', { title: 'Shopping Page Edit' });
+};
+
+/* MESSAGES */
 
 exports.messages = function(req, res){
   res.render('pages/messages/index', { title: 'Messages' });
 };
 
+/* RESERVATIONS */
+
+exports.reservations = function(req, res){
+  res.render('pages/reservations/index', { title: 'Reservations' });
+};
+
+exports.reservationsManage = function(req, res){
+  res.render('pages/reservations/manage/index', { title: 'Reservations manage' });
+};
+
+/* SHOPPINGS */
+
+exports.shoppings = function(req, res){
+  res.render('pages/shoppings/index', { title: 'Shoppings' });
+};
+
+exports.shoppingsPayment = function(req, res){
+  res.render('pages/shoppings/payment/index', { title: 'Shoppings payment' });
+};
+
+exports.shoppingsPurchases = function(req, res){
+  res.render('pages/shoppings/purchases/index', { title: 'Shoppings purchases' });
+};
+
+/* TASKS */
+
+exports.tasks = function(req, res){
+  res.render('pages/tasks/index', { title: 'Tasks' });
+};
+
+/* USERS */
+
 exports.user = function(req, res){
-  res.render('pages/user/index', { title: 'Profile' });
+  res.render('pages/user/index', { title: 'My profile' });
 };
 
-exports.useredit = function(req, res){
-  res.render('pages/user/edit/index', { title: 'Profile' });
+exports.userContacts = function(req, res){
+  res.render('pages/user/contacts/index', { title: 'Contacts' });
 };
 
-exports.usercontacts = function(req, res){
-  res.render('pages/user/contacts/index', { title: 'Profile' });
+exports.userCreate = function(req, res){
+  res.render('pages/user/create/index', { title: 'Create user' });
 };
+
+exports.userEdit = function(req, res){
+  res.render('pages/user/edit/index', { title: 'Edit profile' });
+};
+
+exports.userLogin = function(req, res){
+  res.render('pages/user/login/index', { title: 'Login' });
+};
+
+exports.userLogin = function(req, res){
+  res.render('pages/user/settings/index', { title: 'Settings' });
+};
+
+exports.userUser = function(req, res){
+  res.render('pages/user/user/index', { title: 'UserName' });
+};
+
